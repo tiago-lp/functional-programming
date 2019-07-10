63c872e6a58516f1555b94ff466136bd7cd502bf
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2019-07-09 23:33:17 -0300

    Terminando a questao 8

--- questions/q-08.hs
+++ questions/q-08.hs
@@ -12,3 +12,28 @@ Output: 10
 -}
 
 -- Solution --
+
+-- irei utilizar o quicksort por ser o algoritmo de ordenacao visto nas aulas da disciplina
+-- e tambem um dos melhores e simples no paradigma funcional
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = quicksort l ++ [x] ++ quicksort r
+    where
+        l  = filter (< x) xs
+        r = filter (>= x) xs
+
+khtSmallest arr k = (quicksort arr) !! (k-1)
+
+-- Exemplo
+-- no terminal> ghci q-08.hs
+-- depois digitar 'main' e apertar enter para rodar o exemplo abaixo
+main = do
+    let arr = [7, 10, 4, 3, 20, 15]
+    let result = khtSmallest arr 3
+    let result2 = khtSmallest arr 4
+    print ""
+    print "array = {12, 13, 14, 20, 50}, k = 3"
+    print (show result)
+    print ""
+    print "array = {12, 13, 14, 20, 50}, k = 4"
+    print (show result2)
