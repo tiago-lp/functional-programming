826a91e50ad7230a2745329dd893121a84a6eb21
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2019-07-10 00:20:15 -0300

    Questao 10 feita

--- questions/q-10.hs
+++ questions/q-10.hs
@@ -5,3 +5,22 @@ que recebe um conjunto de elementos guardados em um array e retorna seu conjunto
 -}
 
 -- Solution --
+
+-- Para esta questao irei utilizar listas, por via das duvidas, antes de iniciar a execucao
+-- irei sempre transformar a lista para uma com ocorrencia unica dos elementos (como um conjunto)
+
+unique xs = [x | (x,y) <- zip xs [0..], x `notElem` (take y xs)]
+
+powerSet [] = [[]]
+powerSet (x:xs) = [x:ps | ps <- powerSet xs] ++ powerSet xs
+
+callPowerSet arr = powerSet (unique arr)
+
+-- Exemplo
+-- no terminal> ghci q-10.hs
+-- depois digitar 'main' e apertar enter para rodar o exemplo abaixo
+main = do
+    let arr = [1,2,3]
+    let result = callPowerSet arr
+    print ""
+    print (show result)
