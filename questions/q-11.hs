e871c38f5a787edc6c3bcfe9c9f3313689b87a7c
tiagolimpe <tiago.pereira@ccc.ufcg.edu.br>
2019-07-10 02:11:35 -0300

    Terminei a 11

--- questions/q-11.hs
+++ questions/q-11.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 {-
 11. Um aluno é representado como uma estrutura contando matrícula, um primeiro nome, um sobrenome,
 período de ingresso e CRA.
@@ -10,3 +12,66 @@ período de ingresso e CRA.
 -}
 
 -- Solution --
+import Data.List
+
+-- letra a) (letra b e c estao sendo testadas no main)
+data Aluno = Aluno {
+  matricula :: String
+, nome :: String
+, sobrenome :: String
+, ingresso :: String
+, cra :: Float
+} deriving (Show, Eq)
+
+unique xs = [x | (x,y) <- zip xs [0..], x `notElem` (take y xs)]
+
+getCRAs [] _ = []
+getCRAs (x:xs) result = [(cra x)] ++ result ++ getCRAs xs result
+
+mediaCRA alunos = (foldr (+) 0 (getCRAs alunos [])) / (genericLength alunos)
+
+getAlunosByCra alunos crax = (crax, (filter (\x -> (cra x) == crax) alunos))
+
+groupByCraAux [] _ _ = []
+groupByCraAux (x:xs) alunos result = result ++ [getAlunosByCra alunos x] ++ groupByCraAux xs alunos result
+groupByCra alunos = groupByCraAux (unique (getCRAs alunos [])) alunos []
+
+-- Exemplo
+-- no terminal> ghci q-11.hs
+-- depois digitar 'main' e apertar enter para rodar o exemplo abaixo
+main = do
+    let aluno = Aluno { matricula = "116210629",
+                        nome = "Tiago",
+                        sobrenome = "L. Pereira",
+                        ingresso = "16.2",
+                        cra = 7.33 }
+    let aluno2 = Aluno { matricula = "123456789",
+                        nome = "Ogait",
+                        sobrenome = "Lima",
+                        ingresso = "16.2",
+                        cra = 7.33 }
+    let aluno3 = Aluno { matricula = "987654321",
+                        nome = "Lima",
+                        sobrenome = "Tiago",
+                        ingresso = "16.2",
+                        cra = 9.13 }
+    let aluno4 = Aluno { matricula = "546548978",
+                        nome = "Teste",
+                        sobrenome = "Silva",
+                        ingresso = "16.2",
+                        cra = 9.13 }
+    let aluno5 = Aluno { matricula = "6658794132",
+                        nome = "Aluno",
+                        sobrenome = "Oliveira",
+                        ingresso = "16.2",
+                        cra = 8.55 }
+
+    let alunos = [aluno, aluno2, aluno3, aluno4, aluno5]
+    print ""
+    --letra b)
+    print "Media dos cras"
+    print (show (mediaCRA alunos))
+    print ""
+    -- letra c)
+    print "Agrupados por cra"
+    print (show (groupByCra alunos))
